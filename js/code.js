1008f5fb444117de5ecadce32403e06c400ffe52
Sakhe12 <sakhemzolisa@gmail.com>
2022-11-09 05:01:34 -0800

    update

---
+++ js/code.js
@@ -0,0 +1,84 @@
+//First approach: make use of function
+// let firstName = prompt("Please enter your name: ");
+// let surname = prompt("Please enter your surname: ");
+// let salary = prompt("Please enter your salary: ",0);
+// document.write(`Name: ${firstName}<br>Surname: ${surname}<br> Salary: R${eval(`${salary}*2`)}`);
+
+// function addition() {
+//     return 9 + 2;
+// }
+
+// document.write(`Sum: ${addition()}`);
+
+// let addition = ()=>{
+//     document.write(8 + 4);
+// }
+
+// addition();
+//Array
+// let students = [
+//     'Gift', 'Asandile',
+//     'Deno', 'Javier',
+//     'Sakhe', 'Mkhuseli',
+//     'John', 'Thando', 'Gift'
+// ];
+// let size = students.length;
+// console.log(students);
+// console.log(students[parseInt((size - 1)/2)])
+// console.log(students);
+// console.log(students[0]);
+// //last element
+// console.log(students[size-2]);
+// //Second last element
+// console.log(students[size-2]);
+
+//Comparison operators
+// let numb1 = 6;
+// let numb2 = "6";
+//console.log(numb1 < numb2);
+//console.log(numb1 <= numb2);
+//console.log(numb1 > numb2);
+//console.log(numb1 != numb2);
+//console.log(numb1 == numb2);
+//console.log(numb1 === numb2);
+// let salary = parseFloat(prompt("Enter your salary: "));
+// let age = parseFloat(prompt("Enter your age: "));
+// let qualified = (salary) >= 5000 && (age > 17);
+// let qualified = !(salary) >= 5000 || (age > 17);
+// document.write(qualified);
+
+//Relational operator: in 
+//let numbers =  [9,7,4,3,15];
+// console.log(7 in numbers);
+
+//Increment and decrement
+// let numb1 = 8;
+// console.log(numb1--);
+// //numb1++;
+// console.log(numb1);
+
+//Ternary operator
+//? :
+// let age = 18;
+// console.log(
+//     age >17 ? "You are qualified" :
+//     "You are not qualified");
+
+// let firstName = prompt("Enter your name: ");
+// let salary = parseFloat(prompt("Enter your salary: "));
+
+// document.write(firstName);
+// document.write(
+//     salary >5000 ? 5000 + 1000 :
+//     5000
+// );
+
+//Membership operator
+// let person = {
+//     name: 'User name',
+//     surname: 'User surname'
+// }
+// let search = 'age'
+// console.log(search in person);
+
+
